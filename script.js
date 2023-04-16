9702b7f6c9be49ab31e407a9a24be53d152ccc8a
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-16 14:25:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let ul = document.getElementsByTagName('ul')[0];
+let d;
+for(let i = 0; i<ul.children.length; i++){
+	if(ul.children[i].id==='level'){
+		d = "The level of the element is: "+(i+1);
+	}
+}
+alert(d);
